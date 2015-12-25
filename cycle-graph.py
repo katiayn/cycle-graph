c9edba3e587018ca4a578467926c69f1c1167063
Kátia Nakamura <katiayn@gmail.com>
2015-12-25 02:10:36 -0200

    Refactoring the function that find the most profitable cycle on the graph.

--- cycle-graph.py
+++ cycle-graph.py
@@ -5,14 +5,14 @@ from collections import namedtuple
 def find_maximum_cycle_from_root(root, dest_list, adj_list, route):
     if not dest_list:
         return -sys.maxsize - 1
-
+    global min_cycle_size
     maximum_cost = -sys.maxsize - 1
     best_route_dst = []
     for edge in dest_list:
         route_edge = route
         if edge not in route_edge:
             if edge.dst == root:
-                if len(route_edge) >= 4 and edge.value > maximum_cost:
+                if len(route_edge) >= min_cycle_size and edge.value > maximum_cost:
                     maximum_cost = edge.value
                     best_route_dst = [edge]
             elif edge.dst in adj_list:
@@ -25,30 +25,43 @@ def find_maximum_cycle_from_root(root, dest_list, adj_list, route):
                     best_route_dst.append(edge)
     return maximum_cost, best_route_dst
 
-with open('graph.json', encoding='utf-8') as json_file:
-    json_data = json.load(json_file)
 
-Edge = namedtuple('Edge', 'src dst value')
+def build_adj_list_by_json(file):
+    with open(file, encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
 
-adj_list = {}
-maximum_cost = -sys.maxsize - 1
+    Edge = namedtuple('Edge', 'src dst value')
 
-for route in json_data:
-    for key, value in route.items():
-        src, dst = key.split('-')
-        edge = Edge(src, dst, value)
-        if src not in adj_list:
-            adj_list[src] = []
-        adj_list[src].append(edge)
+    adj_list = {}
 
-for src in adj_list:
-    cost_src, route_src = find_maximum_cycle_from_root(src, adj_list[src], adj_list, [])
-    if cost_src > maximum_cost:
-        best_route = route_src
-        maximum_cost = cost_src
-        print('Most profitable for Source ' + src + ':' + str(maximum_cost))
+    for route in json_data:
+        for key, value in route.items():
+            src, dst = key.split('-')
+            edge = Edge(src, dst, value)
+            if src not in adj_list:
+                adj_list[src] = []
+            adj_list[src].append(edge)
+    return adj_list
 
-print('Most profitable cycle on the graph: ' + str(maximum_cost))
-print('The best route: ')
-for edge in best_route[::-1]:
-    print('Source '+ edge.src + ' to Destination ' + edge.dst + ' - Value: ' + str(edge.value))
+def find_maximum_cycle(adj_list):
+    maximum_cost = -sys.maxsize - 1
+    global best_route
+    for src in adj_list:
+        cost_src, route_src = find_maximum_cycle_from_root(src, adj_list[src], adj_list, [])
+        if cost_src > maximum_cost:
+            best_route = route_src
+            maximum_cost = cost_src
+    return maximum_cost, best_route
+
+min_cycle_size = 4
+adj_list = build_adj_list_by_json('graph.json')
+best_route = []
+maximum_cost, best_route = find_maximum_cycle(adj_list)
+
+if best_route:
+    print('Most profitable cycle on the graph: ' + str(maximum_cost))
+    print('The best route: ')
+    for edge in best_route[::-1]:
+        print('From Source '+ edge.src + ' to Destination ' + edge.dst + ' - Value: ' + str(edge.value))
+else:
+    print('There is no most profitable cycle on the graph with minimum cycle size equals ' + str(min_cycle_size))
